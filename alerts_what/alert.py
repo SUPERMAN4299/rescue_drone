7e03918e9530893cbe4bfb34b502d22d73fc569b
SUPERMAN4299 <abhinav429912345@gmail.com>
2026-05-25 18:28:11 +0530

    sending message of hount

--- alerts_what/alert.py
+++ alerts_what/alert.py
@@ -1,9 +1,13 @@
 import json
-from twilio.rest import Client 
+import time
+from twilio.rest import Client
 
 # Load config
 with open("config.json", "r") as file:
     config = json.load(file)
+
+with open("../camera_ana/human_count.txt", "r") as file:
+    human_count = file.read().strip()
 
 # Twilio credentials
 account_sid = config["account_sid"]
@@ -16,11 +20,13 @@ TO_WHATSAPP = "whatsapp:" + config["TO_WHATSAPP"]
 client = Client(account_sid, auth_token)
 
 # Send message
-message = client.messages.create(
-    from_=FROM_WHATSAPP,
-    body="🚨 ALERT: Human detected by drone!",
-    to=TO_WHATSAPP
-)
+while True:
+    message = client.messages.create(
+        from_=FROM_WHATSAPP,
+        body=f"🚨 ALERT: {human_count} Human detected by drone in the frame!",
+        to=TO_WHATSAPP
+    )
 
-print("Message sent!")
-print("SID:", message.sid)
+    print("Message sent!")
+    print("SID:", message.sid)
+    time.sleep(120)
